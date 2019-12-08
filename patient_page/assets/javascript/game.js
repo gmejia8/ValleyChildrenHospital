c22956d66254475965fdccf8f2639cc5cdc934f8
gmejia8 <46811523+gmejia8@users.noreply.github.com>
2019-12-07 22:44:53 -0800

    Acuity Exam
    Working visual acuity exam letter font change!!

--- patient_page/assets/javascript/game.js
+++ patient_page/assets/javascript/game.js
@@ -4,6 +4,14 @@ var lossCount = 0;
 var guessesRemaining = 1;
 var guessedLetters = [];
 
+//to generate a random font number for the exam letters
+var randomFont = Math.floor(Math.random() *200 + 10);
+console.log(randomFont);
+//to connect to css variable
+let root = document.documentElement;
+//to change the font size value in css
+root.style.setProperty('--fontSize', randomFont + "px");
+
 // set up array for computer to choose from
 var alphabet =
     ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
@@ -13,12 +21,15 @@ var alphabet =
 var randomIndex = Math.floor(Math.random() * alphabet.length);
 var computerChoice = alphabet[randomIndex];
 
+//to display value in html
 document.getElementById("computerChoice").innerHTML = computerChoice;
 console.log(computerChoice);
 
 // add a listener for the user to press a key
 document.onkeyup = function(event) {
   var userChoice = event.key;
+
+
   // make sure the user selects a value a-z
   var regexp = /[a-z]/gi;
     if (!regexp.test(userChoice)) {
@@ -27,42 +38,47 @@ document.onkeyup = function(event) {
     else {
       console.log(userChoice);
     }
+
+
     // reset computer choice if the user loses
     if (computerChoice != userChoice) {
       lossCount++;
       document.getElementById("lossCount").innerHTML = lossCount++;
-      console.log("You lost!");
-      alert("You lost!");
+      console.log("Incorrect");
+      //alert("Incorrect");
       guessesRemaining = 1;
       guessedLetters = [];
-      //document.getElementById("guessedLetters").innerHTML = guessedLetters;
-      //document.getElementById("guessesRemaining").innerHTML = 1;
+      
       randomIndex = Math.floor(Math.random() * alphabet.length);
       computerChoice = alphabet[randomIndex];
       console.log(computerChoice);
       document.getElementById("computerChoice").innerHTML = computerChoice;
+
+      //to generate a random font number for the exam letters
+      var randomFont = Math.floor(Math.random() *200 + 10);
+      console.log(randomFont);
+      //to change the font size value in css
+      root.style.setProperty('--fontSize', randomFont + "px");
     }
+
+
     // compares the randomly selected computer choice and user choice
     if (computerChoice === userChoice) {
-      console.log("You won!");
-      alert("You won!");
+      console.log("Correct");
+      //alert("Correct");
       document.getElementById("winCount").innerHTML = winCount++;
       guessedLetters = [];
-      //document.getElementById("guessedLetters").innerHTML = guessedLetters;
+      
       randomIndex = Math.floor(Math.random() * alphabet.length);
       computerChoice = alphabet[randomIndex];
       console.log(computerChoice);
       document.getElementById("computerChoice").innerHTML = computerChoice;
       guessesRemaining = 1;
-      //document.getElementById("guessesRemaining").innerHTML = 1;
+
+      //to generate a random font number for the exam letters
+      var randomFont = Math.floor(Math.random() *200 + 10);
+      console.log(randomFont);
+      //to change the font size value in css
+      root.style.setProperty('--fontSize', randomFont + "px");
     } 
-    /*else {
-      console.log("Guess again!");
-      document.getElementById("guessesRemaining").innerHTML = guessesRemaining--;
-      guessedLetters.push(userChoice);
-      document.getElementById("guessedLetters").innerHTML = guessedLetters;
-      document.getElementById("computerChoice").innerHTML = computerChoice;
-      
-    }
-    */
 }
